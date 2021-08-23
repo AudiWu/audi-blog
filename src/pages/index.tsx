10e25d305a9f2b6034cb6a11c076f6d98c5fb42b
Audi Wu <audiwu@Audis-MacBook-Air.local>
2021-08-23 16:35:33 +0800

    feat: add styled-component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from "styled-components";
+
+const CustomText = styled.p`
+  color: blue;
+  size: 20px;
+`;
 
 const Index: React.FC = () => (
   <>
     <h1>Hello Wordl!</h1>
     <p>using typescript to write the web</p>
+    <CustomText>This is custom text</CustomText>
   </>
 );
 
